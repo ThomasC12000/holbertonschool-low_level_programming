698185cbfad126377785e5f4bbb39f0085bb4ef0
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-19 18:25:15 +0000

    prints the alphabet with _putchar func.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
