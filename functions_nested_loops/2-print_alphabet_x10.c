036b842e975dd28a9456491f70206ca8a888debe
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-19 18:51:08 +0000

    print alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rint_alphabet_x10 - prints 10 times the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints alphabet 10 times in lowercase, followed by a new line.
  */
 void print_alphabet_x10(void)
 {
