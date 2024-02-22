88dadf139e35fd71c4dbdb3e87e51fcc31012125
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-22 08:23:47 +0000

    update main.h and make task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs -  prints the sign of a number.
+ * @n: The number.
+ * Return: absolute value of an integer.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
