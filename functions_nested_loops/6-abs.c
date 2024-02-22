81cab1adebd3cd32662b778fc6f7fcba67acfc54
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-22 07:51:28 +0000

    update main.h and task 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs -  prints the sign of a number.
+ * Return: absolute value of an integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+}
