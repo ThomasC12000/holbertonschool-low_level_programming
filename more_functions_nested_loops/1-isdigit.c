ff91eb3861ff42c370730ff48099c4390e6fced9
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-26 08:49:22 +0000

    checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: The character to be checked.
+ * Return: 1 if 'c' is uppercase, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+
+	else
+		return (0);
+}
