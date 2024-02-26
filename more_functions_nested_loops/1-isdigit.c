7ca65437d51796aca566e7decf7ff2042302f2f2
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-26 08:54:54 +0000

    update betty doc. 1_isdigit.c and make task 2-mul.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - Checks for a digit.
- * @c: The character to be checked.
+ * @c: The number to be checked.
  * Return: 1 if 'c' is a digit, 0 otherwise.
  */
 int _isdigit(int c)
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: Number.
+ * @b: Number.
+ * Return: sum of two integer.
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+	return(sum);
+}
