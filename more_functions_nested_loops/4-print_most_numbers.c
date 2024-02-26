17a6863e9d1d2dc802360c254d74e41f5aff2b91
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-26 09:53:40 +0000

    update betty doc.

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers 0 to 9, without 2 and 9, foll. a new line.
+ * print_most_numbers - Prints numbers 0 to 9, without 2 and 9.
  */
 void print_most_numbers(void)
 {
