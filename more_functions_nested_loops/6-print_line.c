c4aacea0cb120e2e6c7a1a97bd0033bf2063eda6
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-26 10:32:12 +0000

    make task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ */
+void print_line(int n)
+{
+	int len;
+
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+				for (len = 0; len < n; len++)
+				{
+					_putchar('_');
+				}
+		_putchar('\n');
+	}
+}
