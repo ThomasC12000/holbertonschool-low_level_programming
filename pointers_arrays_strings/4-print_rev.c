0fc97531b038ca564e114c77b562febc27282227
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-02-27 13:43:28 +0000

    update code, use while fonc.

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,13 @@
 void print_rev(char *s)
 {
 	int i;
+	i = 0;
 
-	for (i = 0; s[i] != '\0'; i++);
-	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	for (i--; i >= 0; i--)
 	_putchar(s[i]);
-
 	_putchar('\n');
 }
