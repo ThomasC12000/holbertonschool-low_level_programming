f682b8d69230ef97463388036574d88d00f1ad9b
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-02 17:07:46 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,30 @@
  */
 int _atoi(char *s)
 {
-		int i = 0;
-		int sign = 1;
-		int result = 0;
-		int digit;
-		int max_div_10 = INT_MAX / 10;
-		int max_mod_10 = INT_MAX % 10;
+	int i;
+	int a, b;
 
-		while (s[i])
+	a = 0;
+	b = -1;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			b *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-				if (s[i] == '-')
-						sign *= -1;
-				if (s[i] >= '0' && s[i] <= '9')
-				{
-						digit = s[i] - '0';
+			if (a < 0)
+				a = (a * 10) - (s[i] - '0');
+			else
+				a = (s[i] - '0') * -1;
 
-						if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
-						{
-								return (sign == 1 ? INT_MAX : INT_MIN);
-						}
-
-						result = result * 10 + digit;
-				}
-				else if (result > 0)
-				{
-						break;
-				}
-				i++;
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
 		}
+	}
+	if (b < 0)
+		a *= -1;
 
-		return (result * sign);
+	return (a);
 }
