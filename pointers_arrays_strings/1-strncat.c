1c9132e92056c390fd741e0232e4120efa4563c3
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-02 16:43:17 +0000

    make task 1 and 2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: String
+ * @src: String
+ * @n: integer
+ * Return: result of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+    
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    while (*src != '\0' && n > 0) {
+        *ptr = *src;
+        ptr++;
+        src++;
+        n--;
+    }
+
+    *ptr = '\0';
+
+    return (dest);
+}
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: String
+ * @src: String
+ * @n: integer
+ * Return: result of dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *ptr = dest;
+    
+    while (*src != '\0' && n > 0) 
+	{
+        *ptr = *src;
+        ptr++;
+        src++;
+        n--;
+    }
+
+    while (n > 0) 
+	{
+        *ptr = '\0';
+        ptr++;
+        n--;
+    }
+    return (dest);
+}
