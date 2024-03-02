1e2c2959ca180ad3e17fed5417d7bc30ad9433b1
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-02 16:46:18 +0000

    update betty style

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,19 +11,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *ptr = dest;
 
-    while (*src != '\0' && n > 0)
+	while (*src != '\0' && n > 0)
 	{
-        *ptr = *src;
-        ptr++;
-        src++;
-        n--;
-    }
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
 
-    while (n > 0)
+	while (n > 0)
 	{
-        *ptr = '\0';
-        ptr++;
-        n--;
-    }
-    return (dest);
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+	return (dest);
 }
