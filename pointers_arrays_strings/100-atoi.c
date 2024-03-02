5ca74a9694669f14252905c5fef79494b98d0f76
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-02 10:51:22 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,59 +1,41 @@
 #include "main.h"
 #include <limits.h>
-#include <stdbool.h> // Ajout de cette inclusion pour utiliser le type bool
 
 /**
- * my_atoi - Convertit une chaîne de caractères en un entier.
- * @s: La chaîne de caractères à convertir.
- * Return: L'entier converti.
+ * _atoi - Entry point.
+ * @s: character to be verified.
+ * Return: convert integer.
  */
-int my_atoi(char *s)
+int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int max_div_10 = INT_MAX / 10;
-	int max_mod_10 = INT_MAX % 10;
-	bool found_digit = false; // Utilisation d'un booléen pour suivre si un chiffre a été trouvé
+		int i = 0;
+		int sign = 1;
+		int result = 0;
+		int digit;
+		int max_div_10 = INT_MAX / 10;
+		int max_mod_10 = INT_MAX % 10;
 
-	// Ignorer les espaces au début de la chaîne
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-
-	// Vérifier le signe
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-
-	// Convertir les chiffres en entier
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		found_digit = true; // Marquer qu'un chiffre a été trouvé
-		int digit = s[i] - '0';
-
-		// Vérifier les débordements
-		if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
+		while (s[i])
 		{
-			return (sign == 1 ? INT_MAX : INT_MIN);
+				if (s[i] == '-')
+						sign *= -1;
+				if (s[i] >= '0' && s[i] <= '9')
+				{
+						digit = s[i] - '0';
+
+						if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
+						{
+								return (sign == 1 ? INT_MAX : INT_MIN);
+						}
+
+						result = result * 10 + digit;
+				}
+				else if (result > 0)
+				{
+						break;
+				}
+				i++;
 		}
 
-		result = result * 10 + digit;
-		i++;
-	}
-
-	// Si aucun chiffre n'a été trouvé, retourner 0
-	if (!found_digit)
-	{
-		return 0;
-	}
-
-	return result * sign;
+		return (result * sign);
 }
