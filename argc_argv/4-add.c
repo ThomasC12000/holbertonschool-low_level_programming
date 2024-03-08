bd566e570b98c07db7b2bad5575a701f45dcafad
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-08 10:27:40 +0000

    make task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,24 +1,37 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed into it.
+ * main - add 2 positive numbers and print the result
  * @argc: argument count
- * @argv: argument vector
- * Return: 0 if Success.
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
+	int total, i;
+	char *p;
+	int num;
 
-	for (i = 0; i < argc; i++)
+	total = 0;
+	if (argc > 1)
 	{
-	printf("%s\n", argv[i]);
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
-
-	for (i = 0; i < argc; i++)
-	{
-		if (argv[i] >= 0)
-		printf("%s\n", argv[i]);
-		}		
+	printf("%d\n", total);
 	return (0);
 }
