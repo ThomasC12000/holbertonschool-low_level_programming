fe8ece0d707977dfe16e3024e9af7271450edced
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-11 09:14:07 +0000

    create _putchar.c, main.h, and make task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: the size of the memory to print.
+ * @c: String
+ * Return: pointer to the array, or NULL if it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size > 0)
+	{
+		p = malloc(size * sizeof(char));
+		if (p == NULL)
+		return (NULL);
+	}
+	else
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
