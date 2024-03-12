f2e052740b08b31ed82a5a75cc58741d200b75e1
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-12 08:34:44 +0000

    update task 1 and make task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * str_concat - copies a string into a new buffer
+ * @s1: the string to copy
+ * @s2: 
+ * Return: pointer to a new buffer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int len, len2, j, k;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	for (len = 0; s1[len] != '\0'; len++)
+	{
+	;
+	}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+	;
+	}
+
+	p = malloc((len + len2) * sizeof(char));
+	
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < len; j++)
+	{
+		p[j] = s1[j];
+	}
+	for (k = 0; k < len2; k++)
+	{
+		p[j + k] = s2[k];
+	}
+	p[j + k] = '\0';
+	return (p);
+}
