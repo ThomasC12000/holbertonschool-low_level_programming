7e0eed51dc37fe5fe73446177d7d080d8f6a8645
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-14 10:16:03 +0000

    make task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *TOMMY;
+	int i, j, k;
+	char *n, *o;
+
+	TOMMY = malloc(sizeof(struct dog));
+
+	if (TOMMY == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+		;
+	}
+
+	n = malloc(sizeof(char) * i + 1);
+
+	if (n == NULL)
+	{
+		free(TOMMY);
+		return (NULL);
+	}
+
+	o = malloc(sizeof(char) * j + 1);
+
+	if (o == NULL)
+	{
+		free(n);
+		free(TOMMY);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+	{
+		n[k] = name[k];
+	}
+	for (k = 0; k <= j; k++)
+	{
+		o[k] = owner[k];
+	}
+
+	TOMMY->name = n;
+	TOMMY->age = age;
+	TOMMY->owner = o;
+
+	return (TOMMY);
+}
