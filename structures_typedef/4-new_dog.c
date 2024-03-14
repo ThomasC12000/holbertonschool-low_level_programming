852a859bbfabb6c9ea245d93b346cb49bf8c2f9e
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-14 12:50:24 +0000

    update task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,25 +23,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; name[i] != '\0'; i++)
-	{
 		;
-	}
 	for (j = 0; owner[j] != '\0'; j++)
-	{
 		;
-	}
-
 	n = malloc(sizeof(char) * i + 1);
-
 	if (n == NULL)
 	{
 		free(n);
 		free(TOMMY);
 		return (NULL);
 	}
-
 	o = malloc(sizeof(char) * j + 1);
-
 	if (o == NULL)
 	{
 		free(o);
@@ -50,17 +42,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (k = 0; k <= i; k++)
-	{
 		n[k] = name[k];
-	}
 	for (k = 0; k <= j; k++)
-	{
 		o[k] = owner[k];
-	}
-
 	TOMMY->name = n;
 	TOMMY->age = age;
 	TOMMY->owner = o;
-
 	return (TOMMY);
 }
