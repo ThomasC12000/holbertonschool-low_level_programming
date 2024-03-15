a51260379a12de14e05765362dd3e256340feaa5
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-15 08:19:23 +0000

    make task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array given number of elements and size
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: Null if error, else pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
