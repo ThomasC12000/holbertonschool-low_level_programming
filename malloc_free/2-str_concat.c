f6288688e4f445eed5d8f6ca49a8b07dca41d72c
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-15 09:08:37 +0000

    update

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,10 +11,10 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	unsigned int len, len2, j, k;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len = 0; s1[len] != '\0'; len++)
 	;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
