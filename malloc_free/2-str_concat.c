27a7c94d6a4c2726af687ff476191cd7e4f7d363
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-15 09:03:23 +0000

    update

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,13 +16,9 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	for (len = 0; s1[len] != '\0'; len++)
-	{
 	;
-	}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{
 	;
-	}
 
 	p = malloc((len + len2 + 1) * sizeof(char));
 	
