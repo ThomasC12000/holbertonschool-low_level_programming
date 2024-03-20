981891a7aedd338d6591a2dd85fc33f9f70034ae
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-03-20 08:22:09 +0000

    update code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,12 +11,16 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 	int sum;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
+	sum = 0;
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
 	va_end(list);
-
 	return (sum);
 }
