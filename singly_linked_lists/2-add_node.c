1b79df85e47c5f87a2ad1f41ab781121d61f1589
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-04 08:51:55 +0000

    update task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,6 +2,21 @@
 #include <string.h>
 
 /**
+ * _strlen - Returns the length of a string
+ * @s: String being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	while (*s++)
+		++i;
+	return (i);
+}
+
+/**
  * add_node - Adds a node at the beginning of a list
  * @head: Pointer to address of a list
  * @str: Pointer to string
@@ -17,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 	return (*head);
