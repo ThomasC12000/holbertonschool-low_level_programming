23ba95322aba420870b919566bc3d74461934e13
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-04 08:49:47 +0000

    update task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: String.
+ * Return: Value of a.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
  * add_node_end - Adds a node at the end of a list
  * @head: Pointer to address of a list
  * @str: Pointer to string
@@ -19,7 +35,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 	new->next = NULL;
 	if (!*head)
 	{
